58c83330faeea5be760779b79975b343f3b4d41f
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-02-20 13:52:44 +0100

    cleanup

--- traits/area.h
+++ traits/area.h
@@ -4,13 +4,12 @@
 #include <variant>
 
 namespace Area {
-  // template<typename ...Ts> float area(std::variant<Ts...> const &obj);
   template<typename T> float area(T const &obj);
 
+  // When called with a variant, forward to implementation of resp. types.
   template<typename ...Ts> float area(std::variant<Ts...> const &obj) {
     return std::visit([](auto const &obj){ return area(obj);}, obj);
   }
-
 }
 
 #endif //AREA_H
